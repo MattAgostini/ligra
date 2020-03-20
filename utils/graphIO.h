680dc88ca411d8fc6794a30d1b40cfd8fa7c85e7
Matt Agostini <matt.agostini95@gmail.com>
2020-03-20 09:50:45 -0400

    reepat

--- utils/graphIO.h
+++ utils/graphIO.h
@@ -547,7 +547,7 @@ namespace benchIO {
     
     long *offsets = Off.A;
 
-    printf("Finished offsets %l %l\n", m, sizeof(m));
+    printf("Finished offsets %ld %l\n", m, sizeof(m));
 
     // pointer to each start of word
     char **SA = newA(char*, m);
